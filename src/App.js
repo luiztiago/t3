a42bdb4289f2d4db31daef72047a128d6537fde4
Luiz Tiago Oliveira <luiztiago@gmail.com>
2019-12-12 18:52:06 -0300

    localStorage and time travel

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React from 'react';
 import { Navbar, Container, Form, Button, Alert } from 'react-bootstrap';
 import ReactLoading from "react-loading";
+import DayPickerInput from 'react-day-picker/DayPickerInput';
 import Trello from 'trello';
 import BoardComponent from './components/BoardComponent.js';
+
+import 'react-day-picker/lib/style.css';
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
+    let apiKey = localStorage.getItem('trelloApiKey') || '';
+    let token = localStorage.getItem('trelloToken') || '';
+
     this.state = {
       loading: false,
+      apiKey: apiKey,
+      token: token,
+      trello: null,
+      boards: [],
+      boardId: "",
+      boardIsSelected: false,
+      errorMsg: "",
+      date: new Date()
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleConnect = this.handleConnect.bind(this);
+    this.handleChangeBoard = this.handleChangeBoard.bind(this);
+    this.handleTrelloCredentials = this.handleTrelloCredentials.bind(this);
+  }
+
+  handleTrelloCredentials(event) {
+    localStorage.clear();
+
+    this.setState({
       apiKey: '',
       token: '',
       trello: null,
       boards: [],
       boardId: "",
-      boardIsSelected: false,
-      errorMsg: ""
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleConnect = this.handleConnect.bind(this);
-    this.handleChangeBoard = this.handleChangeBoard.bind(this);
+      boardIsSelected: false
+    });
   }
 
   handleChangeBoard(event) {
@@ -37,9 +59,25 @@ class App extends React.Component {
     this.setState({[event.target.name]: event.target.value});
   }
 
-  handleConnect(event) {
-    event.preventDefault();
+  handleDateChange(day) {
+    this.setState({
+      loading: true,
+      boardIsSelected: false,
+      date: day
+    }, () => {
+      this.setState({ boardIsSelected: true, loading: false });
+    });
+  }
+
+  handleConnect(event = null) {
+    if (event) {
+      event.preventDefault();
+    }
+
     this.setState({loading: true});
+
+    localStorage.setItem('trelloApiKey', this.state.apiKey);
+    localStorage.setItem('trelloToken', this.state.token);
 
     let trello = new Trello(this.state.apiKey, this.state.token);
     this.setState({trello: trello}, () => {
@@ -60,7 +98,9 @@ class App extends React.Component {
           {this.state.trello && this.state.boards !== [] &&
             <Form inline onSubmit={() => {}}>
               <Form.Group controlId="exampleForm.ControlSelect1">
-                <Form.Control as="select" value={this.state.boardId} name="boardId" onChange={this.handleChangeBoard}>
+                <Button variant="link" type="button" size="sm" onClick={this.handleTrelloCredentials}>Change Trello Credentials</Button>
+                <DayPickerInput name="date" value={this.state.date} onDayChange={this.handleDateChange} placeholder="mm/dd/yyyy" />
+                <Form.Control as="select" value={this.state.boardId} name="boardId" onChange={this.handleChangeBoard} disabled={this.state.boardId}>
                   <option key="" value="-1">- Select Board -</option>,
                   {this.state.boards.map(board =>
                     <option key={board.id} value={board.id}>{board.name}</option>
@@ -72,7 +112,7 @@ class App extends React.Component {
         </Navbar>
         <div className="App-content">
           {this.state.boardIsSelected !== false ? (
-            <BoardComponent trello={this.state.trello} id={this.state.boardId} changeBoard={this.handleChangeBoard} loading={this.state.loading} />
+            <BoardComponent trello={this.state.trello} id={this.state.boardId} date={this.state.date} changeBoard={this.handleChangeBoard} loading={this.state.loading} />
           ): (
             <Container className="pt-5">
               {this.state.loading ? <ReactLoading type="bars" /> : (
@@ -86,7 +126,7 @@ class App extends React.Component {
 
                     <Form.Group controlId="formBasicToken">
                       <Form.Label>Token</Form.Label>
-                      <Form.Control value={this.state.token} type="text" name="token" placeholder="Token" onChange={this.handleChange} />
+                      <Form.Control value={this.state.token} type="password" name="token" placeholder="Token" onChange={this.handleChange} />
                     </Form.Group>
 
                     <Button variant="primary" type="submit" size="lg" block disabled={!this.state.apiKey || !this.state.token}>
--- src/components/BoardComponent.js
+++ src/components/BoardComponent.js
@@ -8,51 +8,84 @@ class BoardComponent extends React.Component {
     this.state = {
       id: this.props.id,
       trello: this.props.trello,
+      boards: null,
       lists: [],
-      data: null,
-      loading: this.props.loading
+      cards: [],
+      cardsData: null,
+      loading: this.props.loading,
+      date: this.props.date
     };
 
     this.handleBoardLists = this.handleBoardLists.bind(this);
     this.handleBoardCards = this.handleBoardCards.bind(this);
-    this.handleBoardLabels = this.handleBoardLabels.bind(this);
 
     this.loadBoardData();
   }
 
   loadBoardData () {
-    let boardLabelsPromise = this.state.trello.getLabelsForBoard(this.state.id);
-    boardLabelsPromise.then((boardLabels) => {
-      this.handleBoardLabels(boardLabels);
-    });
-  }
-
-  handleBoardLabels (data) {
     let boardCardsPromise = this.state.trello.getCardsOnBoardWithExtraParams(this.state.id, { members: true, member_fields: 'fullName,avatarUrl', tags: true});
     boardCardsPromise.then((boardCards) => {
-      this.handleBoardCards(boardCards);
+      this.setState({cardsData: boardCards});
+      this.handleBoardCards();
     });
   }
 
-  handleBoardCards (cards) {
+  handleBoardCards () {
     let lists = [];
 
-    if (cards) {
-      cards.map(card => {
-        if (!lists[card.idList]) {
-          lists[card.idList] = [];
-        }
-
-        lists[card.idList].push({
+    if (this.state.cardsData) {
+      this.state.cardsData.map((card) => {
+        this.state.cards[card.id] = {
           id: card.id,
           title: card.name,
           description: card.members.map(member => member.fullName).join(' - ')
-        });
+        };
+
+        return;
       });
     }
 
     this.setState({lists: lists});
 
+    this.loadBoardActions();
+  }
+
+  loadBoardActions () {
+    let dateAtEndOfDay = new Date(this.state.date.setHours(23,59,59,999));
+    let boardActionsPromise = this.state.trello.getActionsOnBoardWithExtraParams(this.state.id, {filter: 'createCard,copyCard,updateCard:idList', before: dateAtEndOfDay, limit: '200'});
+    boardActionsPromise.then((boardActions) => {
+      this.handleBoardActions(boardActions);
+    });
+  }
+
+  handleBoardActions (boardActions) {
+    let cardIds = [];
+
+    let actions = boardActions.filter(action => {
+      let isTheLastActionForCard = false;
+      action.date = new Date(action.date);
+
+      if (!cardIds.includes(action.data.card.id)) {
+        isTheLastActionForCard = true;
+        cardIds.push(action.data.card.id);
+      }
+
+      return (isTheLastActionForCard == true);
+    });
+
+    actions.map(action => {
+      let card = action.data.card;
+      if (!this.state.lists[card.idList]) {
+        this.state.lists[card.idList] = [];
+      }
+
+      this.state.lists[card.idList].push(this.state.cards[card.id]);
+    });
+
+    this.loadBoardLists();
+  }
+
+  loadBoardLists () {
     let boardListsPromise = this.state.trello.getListsOnBoard(this.state.id);
     boardListsPromise.then((boardLists) => {
       this.handleBoardLists(boardLists);
@@ -82,7 +115,7 @@ class BoardComponent extends React.Component {
     return (
       <div>
         {this.state.data && (
-          <Board data={this.state.data} cardStyle={{whiteSpace: 'normal'}} hideCardDeleteIcon={true} />
+          <Board data={this.state.data} cardStyle={{whiteSpace: 'normal'}} hideCardDeleteIcon={true} style={{backgroundColor: '#282c34'}} />
         )}
       </div>
     )
